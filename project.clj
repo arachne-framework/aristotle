53918912613f203c156e889c63b339fbea6e3ac2
Luke VanderHart <luke@vanderhart.net>
2017-10-06 10:17:49 -0400

    initial commit of empty project

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject org.arachne-framework/aristotle "0.1.0-SNAPSHOT"
+  :description "RDF and Ontologies for Clojure, wrapping Apache Jena"
+  :url "http://github.com/arachne-framework/aristotle"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-beta1"]])
